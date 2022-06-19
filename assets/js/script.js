a7e61c49ebba0626e54728c83723bb4eb88487f3
kelsie kidd <kelsiekidd93@gmail.com>
2022-06-19 17:33:49 -0500

    reset the form

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,15 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var taskFormHandler = function(event) {
     event.preventDefault();
-var taskNameInput = document.querySelector("input[name='task-name']").value;
-var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+// check if input values are empty strings
+  if (!taskNameInput || !taskTypeInput) {
+  alert("You need to fill out the task form!");
+  return false;
+}
+  formEl.reset();
 
 //package up data as an object
 var taskDataObj = {
@@ -18,11 +25,11 @@ createTaskEl(taskDataObj);
 
 var createTaskEl = function(taskDataObj) {
   //create list item
-var listItemEl = document.createElement("li");
+  var listItemEl = document.createElement("li");
 listItemEl.className = "task-item";
 
 // create div to hold task info and add to list item
-var taskInfoEl = document.createElement("div");
+  var taskInfoEl = document.createElement("div");
 
 //give it a class name
 taskInfoEl.className = "task-info";
